ecd395358530d744d0def8776245acfdbc59236b
Diego Cabo <dcgdiego@gmail.com>
2018-10-08 00:56:03 +0200

    Few style changes to my home page

--- SES/frontend/src/containers/MyHouse.js
+++ SES/frontend/src/containers/MyHouse.js
@@ -6,14 +6,26 @@ class MyHouse extends React.Component {
 
     state = {
         money: 0,
-        battery: 0
+        battery: 0,
+        energyConsumption: 0,
+        energyProduction: 0,
+        stove: 0,
+        lights: 0,
+        householdAppliances: 0,
+        homeEntertainment: 0
     }
 
 
     componentDidMount() {
         axios.all([
             axios.get('http://127.0.0.1:8000/api/money/1'),
-            axios.get('http://127.0.0.1:8000/api/battery/1')
+            axios.get('http://127.0.0.1:8000/api/battery/1'),
+            axios.get('http://127.0.0.1:8000/api/energyconsumption/1'),
+            axios.get('http://127.0.0.1:8000/api/energyproduction/1'),
+            axios.get('http://127.0.0.1:8000/api/stove/1'),
+            axios.get('http://127.0.0.1:8000/api/lights/1'),
+            axios.get('http://127.0.0.1:8000/api/householdappli/1'),
+            axios.get('http://127.0.0.1:8000/api/entertainment/1')
           ])
           .then(axios.spread((moneyRes, batteryRes) => {
             this.setState({ money: moneyRes.data.amount, battery: batteryRes.data.level });
@@ -32,9 +44,7 @@ class MyHouse extends React.Component {
         <div class="row">
             <div class="col-md-12">
                 <div class="jumbotron">
-                    <h2>
-                        My house
-                    </h2>
+                    <h2>My house</h2>
                     <p>
                        View all information about your houses energy consumption and production here.
                     </p>
@@ -45,16 +55,16 @@ class MyHouse extends React.Component {
         <div class="row">
             <div class="col-md-6">
             <center>
-            <h3>
-                Energy consumption  
-            </h3>
+                <h3>Energy consumption</h3>
+                <p style={{color: "red"}}>{this.state.energyConsumption} kW·h</p> 
+                <img src="img/consumptionSymbol.svg" alt="Slate Bootstrap Admin Theme" width={100} height={100} />
             </center>
             </div>
             <div class="col-md-6">
             <center>
-            <h3>
-                Energy production
-            </h3>
+                <h3>Energy production</h3>
+                <p style={{color: "green"}}>{this.state.energyProduction} kW·h</p> 
+                <img src="img/productionSymbol.svg" alt="Slate Bootstrap Admin Theme" width={100} height={100} />
             </center>
             </div>
         </div>
@@ -64,16 +74,14 @@ class MyHouse extends React.Component {
         <div class="row">
         <center>
             <div class="col-md-6">
-            <h3>
-                Money 
+                <h3>Money</h3>
                 <p>{this.state.money}</p>
-            </h3>
+                <img src="img/moneySymbol.svg" alt="Slate Bootstrap Admin Theme" width={80} height={80} />
             </div>
             <div class="col-md-6">
-            <h3>
-                Battery
-                <p>{this.state.battery}</p>
-            </h3>
+                <h3>Battery</h3>
+                <p style={(this.state.battery < 15)? {color: "orange"}:{color: "green"}}>{this.state.battery} kW·h</p> 
+                <img src="img/batterySymbol.svg" alt="Slate Bootstrap Admin Theme" width={80} height={80} />
             </div>
             </center>
         </div>
@@ -83,24 +91,24 @@ class MyHouse extends React.Component {
         <div class="row">
         <center>
             <div class="col-md-3">
-            <h3>
-                Stove
-            </h3>                
+                <h3>Stove</h3>
+                <p>{this.state.stove}</p>
+                <img src="img/stoveSymbol.svg" alt="Slate Bootstrap Admin Theme" width={80} height={80} />
             </div>
             <div class="col-md-3">
-            <h3>
-                Lights
-            </h3>
+                <h3>Lights</h3>
+                <p>{this.state.lights}</p>
+                <img src="img/lightSymbol.svg" alt="Slate Bootstrap Admin Theme" width={80} height={80} />
             </div>
             <div class="col-md-3">
-            <h3>
-                Cleaning  
-            </h3>
+                <h3>Household Appliances</h3>
+                <p>{this.state.householdAppliances}</p>
+                <img src="img/cleaningSymbol.svg" alt="Slate Bootstrap Admin Theme" width={80} height={80} />
             </div>
             <div class="col-md-3">
-            <h3>
-                Home entertainment
-            </h3>
+                <h3>Home entertainment</h3>
+                <p>{this.state.homeEntertainment}</p>
+                <img src="img/entertainmentSymbol.svg" alt="Slate Bootstrap Admin Theme" width={80} height={80} />
             </div>
             </center>
         </div>
